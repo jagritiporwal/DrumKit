1c5d1389d892caf1ba3f37cb5c1cbc1eaeb78fbe
jagritiporwal <40630211+jagritiporwal@users.noreply.github.com>
2020-06-26 11:20:15 +0530

    Initial Commit

---
+++ drum.js
@@ -0,0 +1,23 @@
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    console.log("taking the key code" + audio);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+}
+window.addEventListener('keydown', playSound);
+
+    function removeTransition(e) {
+        if (e.propertyName !== "transform")
+            return;
+        this.classList.remove('playing');
+
+    }
+
